cb4cdcc77b4c538d34549c1ee8838cbeb8a5da5b
paulovna <vna.paulo@gmail.com>
2022-10-07 10:15:24 -0300

    Testes de quantidade de notas para todos os valores

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TrocoTest.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TrocoTest.java
@@ -8,5 +8,31 @@ public class TrocoTest {
         Troco troco = new Troco(126.89);
         assertEquals(troco.papeisMoeda[5].getQuantidade(),1);;
     }
+    @Test
+    public void testeQuantidade50(){
+        Troco troco = new Troco(176.89);
+        assertEquals(troco.papeisMoeda[4].getQuantidade(),1);;
+    }
+    @Test
+    public void testeQuantidade20(){
+        Troco troco = new Troco(126.89);
+        assertEquals(troco.papeisMoeda[3].getQuantidade(),1);;
+    }
+    @Test
+    public void testeQuantidade10(){
+        Troco troco = new Troco(111.89);
+        assertEquals(troco.papeisMoeda[2].getQuantidade(),1);;
+    }
+    @Test
+    public void testeQuantidade5(){
+        Troco troco = new Troco(106.89);
+        assertEquals(troco.papeisMoeda[1].getQuantidade(),1);;
+    }
+    @Test
+    public void testeQuantidade2(){
+        Troco troco = new Troco(102.89);
+        assertEquals(troco.papeisMoeda[0].getQuantidade(),1);;
+    }
+
 }
 

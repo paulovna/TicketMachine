36210c22c45e1a07e743658bec89f050caa9905b
paulovna <vna.paulo@gmail.com>
2022-10-07 09:26:35 -0300

    Primeiro Teste: criação PapelMoeda

--- Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoedaTest.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoedaTest.java
@@ -1,10 +1,11 @@
 package br.calebe.ticketmachine.core;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-/**
- *
- * @author Calebe de Paula Bianchini
- */
 public class PapelMoedaTest {
-
-
+    @Test
+    public void criacaoPapelMoeda(){
+        PapelMoeda papelMoeda = new PapelMoeda(5);
+        assertEquals(papelMoeda.getQuantidade(),0);;
+    }
 }

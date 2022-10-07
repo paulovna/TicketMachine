9ad4a07bfd7d23159e32bb673b7ee4c05270197c
Kaique <48844732+unifkaraujo@users.noreply.github.com>
2022-10-07 19:29:40 -0300

    Teste: instanciaPreco()

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachineTest.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachineTest.java
@@ -4,5 +4,9 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 
 public class TicketMachineTest {
-       
+    @Test
+    public void instanciaPreco(){
+        TicketMachine tm = new TicketMachine(5);
+        assertEquals(tm.precoDoBilhete, 5);;
+    }  
 }

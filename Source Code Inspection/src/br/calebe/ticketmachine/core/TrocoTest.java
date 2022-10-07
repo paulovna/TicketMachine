dff8de63b1aa638249f0d145d857fe7f0d1eebf2
paulovna <vna.paulo@gmail.com>
2022-10-07 10:08:29 -0300

    Quarto Teste: Quantidade de notas de R$100,00

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TrocoTest.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TrocoTest.java
@@ -2,6 +2,11 @@ package br.calebe.ticketmachine.core;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-class TrocoTest {
-
+public class TrocoTest {
+    @Test
+    public void testeQuantidade100(){
+        Troco troco = new Troco(126.89);
+        assertEquals(troco.papeisMoeda[5].getQuantidade(),1);;
+    }
 }
+

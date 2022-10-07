88d8459ce7a7b560ff21381cf17508cca133158e
Kaique <48844732+unifkaraujo@users.noreply.github.com>
2022-10-07 19:28:11 -0300

    Alteração da classe TicketMachineTest

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachineTest.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachineTest.java
@@ -1,13 +1,8 @@
 package br.calebe.ticketmachine.core;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import org.junit.Test;
 
 public class TicketMachineTest {
-    @Test
-    public void testeQuantidadeNotas100(){
-        Troco troco = new Troco(126.89);
-        assertEquals(troco.papeisMoeda[5].getQuantidade(),1);;
-    }
-    
+       
 }
-
